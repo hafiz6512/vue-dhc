06fa77e88fea0250a501c0f1bc8aaebd354cbe8b
Hafizur Rahman <hafiz6512@gmail.com>
2023-01-23 16:59:44 +0600

    Add Welcome Massage

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="DHC logo" src="./assets/images/dhc-logo.png">
+    <HelloDHC msg="A welcome message at Diehardcoder"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloDHC from './components/HelloDHC.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            HelloDHC
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/HelloDHC.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="welcome-msg">
+
+    <h1>{{ msg }}</h1>
+
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloDHC',
+        props: {
+            msg: String
+        }
+    }
+</script>
+
